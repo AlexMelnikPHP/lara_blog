711cf421462bf4fca6b3b939f66fa31b59b6b205
AlexMel <alexey.melnik2000@gmail.com>
2020-07-10 19:13:43 +0300

    first commit

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Sidebar -->
+<nav id="sidebar">
+
+
+    <ul class="list-unstyled components">
+
+
+
+        <li>
+            <a href="home.html">Home</a>
+        </li>
+        <li>
+          <a href="newblog.html">Create Blog</a>
+        </li>
+        <li>
+            <a href="settings.html">Site Settings</a>
+        </li>
+    </ul>
+
+</nav>
